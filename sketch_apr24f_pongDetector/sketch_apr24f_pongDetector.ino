d2b0c18e181d65a835dc3f28cea14f12e56a5b36
Taylor Whitfield <twhitfield@thecargoagency.com>
2021-05-02 15:43:32 -0400

    WIP tracking Left and Right separately

--- sketch_apr24f_pongDetector/sketch_apr24f_pongDetector.ino
+++ sketch_apr24f_pongDetector/sketch_apr24f_pongDetector.ino
@@ -1,3 +1,5 @@
+#include <KnockDetector.h>
+
 
 //TODO
 //- Start game using secret knock.
@@ -5,16 +7,14 @@
 //- Or just make two buttons under the table
 
 
-#include <KnockDetector.h>
-
 //const int buttonPin = A8;     // the number of the pushbutton pin
 //int buttonState = 0;         // variable for reading the pushbutton status
 
 
 const int piezoInputPinOne = A0;
 const int piezoInputPinTwo = A7;
-
-//int piezoInputSignalOne = 0;
+const int piezoInputPins[2] = {A0, A7}; //using a constant would keep you from making your error
+//int result[2];
 
 //Need to play a round and gather data to see what the threshold should actually be.
 //const int pulseLength = 12345;
@@ -46,12 +46,15 @@ int lkp = 0;
 void callback(float knockIntensity, long pulseLength) {
 
   if (knockIntensity >= intensityThreshold) {
+    //    (lastPin == 0) ? expression1 : expression2;
+
+
     rallyPoints = rallyPoints + 1;
     //  Serial.println("Knock detected!");
-      Serial.print("Intensity: ");
-      Serial.println(knockIntensity);
     //  Serial.print("Pulse length: ");
     //  Serial.println(pulseLength);
+    Serial.print("Intensity: ");
+    Serial.println(knockIntensity);
 
 
     if (!weAreTiming) {
@@ -61,7 +64,7 @@ void callback(float knockIntensity, long pulseLength) {
       Serial.println(firstEventAt);
     }
     else {
- 
+
       secondEventAt = millis();
       //      Serial.println(secondEventAt);
       timeBetweenEvents = secondEventAt - firstEventAt;
@@ -84,15 +87,17 @@ void callback(float knockIntensity, long pulseLength) {
 
         Serial.print(" ");
       }
-           weAreTiming = false;
+      weAreTiming = false;
       //      Serial.print(" end   ");
     }
 
 
   }
 }
+//These numbers are -> (int lowThreshold, int noiseThreshold, int pin, CALLBACK)
+KnockDetector knockDetectorOne(20, 5, callback);
+KnockDetector knockDetectorTwo(20, 5, callback);
 
-KnockDetector knockDetector(20, 5, callback);
 
 void setup() {
   Serial.begin(115200);
@@ -100,34 +105,34 @@ void setup() {
 }
 
 void loop() {
-  knockDetector.loop(analogRead(piezoInputPinOne));
-  knockDetector.loop(analogRead(piezoInputPinTwo));
+  if (analogRead(piezoInputPinOne) > 1) {
+    lkp = 0;
+    Serial.print("Last Pin: LEFT  ");
+    Serial.println(lkp);
+    Serial.println(analogRead(piezoInputPinOne));
+  }
+  else if (analogRead(piezoInputPinTwo) > 1 ) {
+    lkp = 1;
+    Serial.print("Last Pin: RIGHT ");
+    Serial.println(lkp);
+    Serial.println(analogRead(piezoInputPinTwo));
+  }
 
-  //  Start a timer once a ralley has started.
-  //  if (rallyPoints > 0) {
-  //    runTime = millis();
 
-  //  If the time between two seperate knocks is greater than 5 seconds, then add the points to the correct players total.
-  //    if((runTime > timeThreshold) && (lkp == 0) ) {
-  //      p1 = p1 + rallyPoints;
-  //      Serial.print("Player 1: ");
-  //      Serial.println(p1);
-  //    }
-  //    else if((runTime > timeThreshold) && (lkp == 1) ) {
-  //      p2 = p2 + rallyPoints;
-  //      Serial.print("Player 2: ");
-  //      Serial.println(p2);
-  //    }
-  //  }
-
-  //  buttonState = digitalRead(buttonPin);
-  //  if (buttonState == HIGH) {
-  //    p1 = p1 + rallyPoints;
-  //    Serial.print("Button Pressed P1: ");
-  //    Serial.println(p1);
-  //  }
-  //  else {
+  //  for (byte y = 0; y < 2; y++) {
+  //    knockDetector.loop(analogRead(piezoInputPins[y]));
   //
+  //  }
+
+
+  //  if (knockDetector.available()) {
+  //  for (byte y = 0; y < 2; y++)
+  //    {
+  //      result[y] =
+  //      Serial.print("\t test ");
+  //      Serial.print(result[y]);
+  //
+  //    }
   //  }
 
 }
